14bab43c050344d7e79698f402b5aa20b5083c04
Kavinda Weerasooriya <87388524+kavindaweerasooriya@users.noreply.github.com>
2022-08-27 15:46:09 +0530

    Added the other functions of the Sensors and devices of the project

--- Code/Code.ino
+++ Code/Code.ino
@@ -83,4 +83,116 @@ if(alarm>50)
  }
 }
 
+
+String getTemperatureValue(){
+
+   dhtObject.read(dht_apin);
+   Serial.print(" Temperature(C)= ");
+   int temp = dhtObject.temperature;
+   Serial.println(temp); 
+   delay(50);
+   return String(temp); 
+  
+}
+
+
+String getHumidityValue(){
+
+   dhtObject.read(dht_apin);
+   Serial.print(" Humidity in %= ");
+   int humidity = dhtObject.humidity;
+   Serial.println(humidity);
+   delay(50);
+   return String(humidity); 
+  
+}
+
+
+String getgasValue(){
+   Serial.print(" Gas(level)= ");
+   int val=analogRead(A0); 
+   Serial.println(val); 
+   delay(50);
+   return String(val); 
+  
+}
+
+int getgastoValue(){
+   Serial.print(" Gas(level)= ");
+   int val=analogRead(A0); 
+   Serial.println(val); 
+   delay(50);
+   return int(val);   
+}
+
+String getfireValue(){
+   Serial.print("fire= ");
+   int val=digitalRead(4); 
+   Serial.println(val); 
+   delay(50);
+   return String(val);  
+}
+
+
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    esp8266.println(command);//at+cipsend
+    if(esp8266.find(readReplay))//ok
+    {
+      found = true;
+      break;
+    }
+  
+    countTimeCommand++;
+  }
+  
+  if(found == true)
+  {
+    Serial.println("OYI");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  
+  found = false;
+ }
+ void sms()
+{
+  Serial.println("Initializing..."); 
+  delay(1000);
+  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  mySerial.println("AT+CMGS=\"+94772303895\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  updateSerial();
+  mySerial.print("Gas Leakage detected"); //text content
+  updateSerial();
+  mySerial.write(26);
+  delay(30000);  
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
+}
  
